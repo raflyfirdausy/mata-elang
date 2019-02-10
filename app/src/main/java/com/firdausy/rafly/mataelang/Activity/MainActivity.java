a398f4476896f146711b43f2a1b808617b3f711b
Rafli Firdausy Irawan <firdausy.irawan@my.gavilan.edu>
2019-02-11 00:50:03 +0700

    update design

--- app/src/main/java/com/firdausy/rafly/mataelang/Activity/MainActivity.java
+++ app/src/main/java/com/firdausy/rafly/mataelang/Activity/MainActivity.java
@@ -3,6 +3,7 @@ package com.firdausy.rafly.mataelang.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.view.MenuInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -24,15 +25,8 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        toolbar.setTitle(R.string.mata_elang);
+        toolbar.setSubtitle(R.string.dashboard);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -56,23 +50,13 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -82,17 +66,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.action_tambahUser) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.action_input) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.action_lihat) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.action_pengaturan) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.action_edit) {
+
+        } else if (id == R.id.action_logout) {
 
         }
 
