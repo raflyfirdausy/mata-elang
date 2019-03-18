61ed9a54393d4be97952081ec1d47ee345ef6ce6
Rafli Firdausy Irawan <firdausy.irawan@my.gavilan.edu>
2019-03-18 15:35:26 +0700

    done halaman pendaftaran posyandu

---
+++ app/src/main/java/com/firdausy/rafly/mataelang/Activity/DaftarPosyanduActivity.java
@@ -0,0 +1,15 @@
+package com.firdausy.rafly.mataelang.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.firdausy.rafly.mataelang.R;
+
+public class DaftarPosyanduActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daftar_posyandu);
+    }
+}
--- app/src/main/java/com/firdausy/rafly/mataelang/Activity/LoginActivity.java
+++ app/src/main/java/com/firdausy/rafly/mataelang/Activity/LoginActivity.java
@@ -104,6 +104,13 @@ public class LoginActivity extends AppCompatActivity {
                 tampilbantuan();
             }
         });
+        tv_klikDisini.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(context, DaftarPosyanduActivity.class));
+            }
+        });
+
 
     }
 
