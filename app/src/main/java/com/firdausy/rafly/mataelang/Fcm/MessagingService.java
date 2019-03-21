b38cb4af94a86c9d9f836b7c8faace8cf2de699e
trian <triannurizkillah@gmail.com>
2019-03-20 17:02:04 -0700

    fix chat di variable position push notif firebase function

---
+++ app/src/main/java/com/firdausy/rafly/mataelang/Fcm/MessagingService.java
@@ -0,0 +1,17 @@
+package com.firdausy.rafly.mataelang.Fcm;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/*
+ * Created by Mahmoud on 3/13/2017.
+ */
+
+public class MessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        //todo: handle notification
+    }
+
+}
